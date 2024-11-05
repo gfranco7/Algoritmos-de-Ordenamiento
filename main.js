7eb474dcd1facd4e9952217aa8fdb8f2134fb2e0
gfranco7 <166555820+gfranco7@users.noreply.github.com>
2024-11-05 18:02:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,38 @@
     let mynumber =prompt('Ingresa un numero: ') 
     let numberInv = 0;
     while (mynumber > 0){
-        numberInv = 10*numberInv+mynumber % 10
+        residuo = mynumber%10
+        numberInv = 10*numberInv+residuo
         mynumber = Math.floor(mynumber/10)    
     }
 
     alert('Numero invertido => '+numberInv) 
 }
-NumberInverted()
+//NumberInverted()
 
+
+
+
+//Escribir una programa que calcule los siguientes 3 número de la sucesión 1, 1, 2, -1, 1, -2, -1, ?, ?, ?
+
+
+async function dardos(){
+    let dardos = [];
+    for (let index = 0; index < 100; index++) {
+        dardos.push((Math.random()*2).toFixed(2))
+    }
+    console.log(dardos)
+
+}
+
+console.log(dardos(10))
+
+
+async function simulacionMC(){
+    let outpoints;
+    let inpoints;
+
+    console.log(Math.sqrt(9))
+}
+
+simulacionMC()
