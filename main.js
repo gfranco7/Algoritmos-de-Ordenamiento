57a748d1181acd8f406d78657b302f87eb96fd93
gfranco7 <166555820+gfranco7@users.noreply.github.com>
2024-11-05 15:30:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
 
-let NumbersList = [3, 7, 1, 9, 4, 6, 2, 8, 5, 10, 15, 12, 18, 14, 11, 17, 20, 19, 16, 13]
 
-async function SORTING(){
-    let topOne = 1;
-    NumbersList.sort((a,b)=>(a-b))
-    console.log(NumbersList)
+
+
+
+
+
+
+
+
+
+
+
+async function NumberInverted(mynumber){ 
+    let residuo;
+    let numberInv;
+    while (mynumber > 0){
+        numberInv = 10*numberInv+n % 10
+        mynumber /=10
+    }
+
+    //division2 = division*
+    console.log(numberInv) 
 }
+NumberInverted(123)
 
-//SORTING();
-
-async function BINARYS(mynumber){
-    let topIndex = NumbersList.length-1;
-    let lowIndex = NumbersList[0];
-    let half;
-    while(lowIndex < topIndex){
-        half = Math.floor(NumbersList.length/2);
-        if (NumbersList[half]==mynumber){
-            return console.log('The number has been found in the '+half+ 'position')
-        }
-        if (mynumber > NumbersList[half]){
-            lowIndex = NumbersList[half]+1
-        }else{
-            topIndex = NumbersList[half] -1
-        }
-    }return console.log('Not found')
-     
-}
-
-BINARYS(2) 
